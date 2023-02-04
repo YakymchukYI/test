f13bc3122443496dfb52ea22b9371fe30d767323
YakymchukYI <yaroslav.yakymchuk27@gmail.com>
2023-02-04 16:47:29 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello
+          Hello111
         </p>
       </header>
     </div>
